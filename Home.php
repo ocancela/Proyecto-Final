874fd6c344c5e19dd1cad941c37d7860656f7039
ocancela <ocancela@yahoo.com>
2019-09-15 00:10:23 -0300

    Primer Commit

---
+++ Home.php
@@ -0,0 +1,75 @@
+<?php
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+
+     	<link rel="stylesheet" href="css/style.css">
+     <title>Home</title>
+   </head>
+<body>
+ <div class="contenedor">
+
+   <header class="main-header">
+     <img src="images/logo.jpg" alt="logotipo" class="logo">
+
+     <nav class="categorias">
+       <ul>
+         <li><a href="#">Trabajo</a></li>
+         <li><a href="#">capacitación</a></li>
+         <li><a href="#">Emprendimientos</a></li>
+         <li><a href="#">Obtener Título</a></li>
+         <li><a href="#">Perfil</a></li>
+         <li><a href="#">contacto</a></li>
+         <li><a href="#">Quiero Ayudar</a></li>
+         <li><a href="#">Recreación</a></li>
+       </ul>
+     </nav>
+   </header>
+
+  <section class="posteos">
+    <article class="post">
+      <div class="photo-container">
+        <img class="photo" src="images/Mandela_2.jpg" alt="foto 01">
+      </div>
+      <h2>Lorem ipsum amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      <a class="more" href="#">ver más</a>
+    </article>
+    <article class="post">
+      <div class="photo-container">
+        <img class="photo" src="images/Mandela_1.jpg" alt="foto 02">
+      </div>
+      <h2>Lorem ipsum amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      <a class="more" href="#">ver más</a>
+    </article>
+    <article class="post">
+      <div class="photo-container">
+        <img class="photo" src="images/Mandela_3.jpg" alt="foto 03">
+      </div>
+      <h2>Lorem ipsum amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      <a class="more" href="#">ver más</a>
+    </article>
+
+  </section>
+
+
+
+
+
+   <footer>
+     <nav class="main-footer">
+       <ul>
+         <li><a href="#">Ingresar</a></li>
+         <li><a href="#">Registro</a></li>
+        </ul>
+     </nav>
+   </footer>
+</div>
+</body>
+</html>
