e55db9cfe400ab5567f130ae5495d5bd8a73489b
Valentina Mammolino <vmammolino@gmail.com>
2019-09-18 16:05:38 -0300

    Arme login, registro y separe archivo perfil para despues armarlo con toda la info cargue estilos de los formularios en styles.css

--- login.php
+++ login.php
@@ -1,53 +1,53 @@
 <?php
+  $datosregistracion = [];
+  include "validar.php";
+
 ?>
 
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="css/styles.css">
-		<title>Comunidad | Login</title>
-	</head>
-	<body>
-		<div class=""form-container""> <!-- Contenedor ppal -->
-			<header> <!-- Encabezadp -->
-				<nav class="main-nav">
-					<a href="index.html"> <img  ="logo" src="images/logo.png" alt="logotipo" class="logo"></a>
-					<ul class="menu">
-						<li><a href="#">Smartphones</a></li>
-						<li><a href="#">Tablets</a></li>
-						<li><a href="#">Computadoras</a></li>
-					</ul>
-					<ul class="menu access">
-						<li><a href="login.html">login</a></li>
-						<li><a href="register.html">registro</a></li>
-					</ul>
-				</nav>
-			</header>
-			<main> <!-- Cuerpo principal del sitio -->
-				<section class="form-container">
-					<h1>Bienvenido</h1>
-					<form class="access-form" action="" method="get">
-						<div class="field-group">
-							<label for="email">Email</label>
-							<input type="email" id="email" name="email" placeholder="Email" value="" required>
-						</div>
-						<div class="field-group">
-							<label for="password">Contraseña</label>
-							<input type="password" id="passsword" name="password" placeholder="password" value="" required>
-						</div>
-						<div class="field-group remember-me">
-							<input type="checkbox" id="remember-me" name="remember-me" value="">
-							<label for="remember-me">Recordarme</label>
-						</div>
-						<p class="form-text alert-hide">
-							La combinación ingresada de email y contraseña no es válida.
-						</p>
-						<button type="submit" name="send">Ingresar</button>
-					</form>
-				</section>
-			</main>
-		</div>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Registracion</title>
+  </head>
 
-	</body>
+  <body>
+    <header  class="main-header">
+      <img  ="logo" src="images/logo.png" alt="logotipo" class="logo">
+    </header>
+
+    <main>
+      <section class="form-container">
+      	<h1>Bienvenido</h1>
+
+	      <form action="Home.php" method="POST">
+	        <div class="field-group">
+	          <label for="username">Nombre de Usuario</label>
+	          <input type="text" name="username" maxlength="15" required><br>
+	        </div>
+
+	        <div class="field-group">
+	          <label for="email">Email</label>
+	          <input type="email" name="email" required><br>
+	        </div>
+
+	        <div class="field-group">
+	          <label for="password">Contraseña</label>
+	          <input type="password" name="password" value="" required><br>
+	        </div>
+					<br>
+					<div class="field-group remember-me">
+						<input type="checkbox" id="remember-me" name="remember-me" value="">
+						<label for="remember-me">Recordarme</label><br>
+					</div>
+
+					<button type="submit" name="send"><strong>Ingresar</strong></button>
+
+	      </form>
+	    </section>
+
+    </main>
+  </body>
 </html>
--- registracion.php
+++ registracion.php
@@ -13,83 +13,45 @@
     <title>Registracion</title>
   </head>
   <body>
-    <header>
-
+    <header  class="main-header">
+      <img  ="logo" src="images/logo.png" alt="logotipo" class="logo">
     </header>
-    <main> <!-- Cuerpo principal del sitio -->
-      <section class="form-container">
 
+    <main>
+      <section class="form-container">
       <h1>Registrate</h1>
+      
       <form action="Home.php" method="POST">
-        Nombre:
-        <input type="text" name="nombre" maxlength="50" required><br><br>
-        Apellido:
-        <input type="text" name="apellido" maxlength="50" required><br><br>
-        Género:
-        <input type="radio" name="genero" value="f">Femenino</input>
-        <input type="radio" name="genero" value="m">Masculino</input><br><br>
-        Fecha de Nacimiento:
-        <input type="date" name="fechanac"><br><br>
+        <div class="field-group">
+          <label for="username">Nombre de Usuario</label>
+          <input type="text" name="username" maxlength="15" required><br>
+        </div>
+
+        <div class="field-group">
+          <label for="email">Email</label>
+          <input type="email" name="email" required><br>
+        </div>
+
+        <div class="field-group">
+          <label for="password">Contraseña</label>
+          <input type="password" name="password" value="" required><br>
+        </div>
+
+        <div class="field-group">
+          <label for="retypepassword">Confirmar Contraseña</label>
+          <input type="password" name="retypepassword" value="" required><br>
+        </div>
+
         <br>
-        Domicilio:
-        <input type="text" name="nombre" maxlength="100"><br><br>
-        Localidad:
-        <select name="localidad">
-            <option value="BE">Benavidez</option>
-            <option value="GA">Garín</option>
-            <option value="GA">Gral Pacheco</option>
-            <option value="GA">La Paloma</option>
-            <option value="GA">Las Tunas</option>
-            <option value="MA">Malvinas Argentinas</option>
-            <option value="PN">Pablo Nogues</option>
-            <option value="RR" selected="selected">Ricardo Rojas</option>
-        </select> <br><br>
-        Celular:
-        <input type="number" name="celular"><br>
+        <input type="checkbox" id="tyc" name="tyc" value="yes" required>
+        <label for="tyc">Acepto los términos y condiciones.</label><br>
         <br>
-        Instituciones Parroquiales a las que perteneces/perteneciste:
-          <br>
-          <input type="checkbox" name="institucion" value="cam">Casa del Niño Amanecer</input><br>
-          <input type="checkbox" name="institucion" value="spc">San Pedro Claver</input><br>
-          <input type="checkbox" name="institucion" value="cai">CAI San Juan Bautista</input><br>
-          <input type="checkbox" name="institucion" value="lyc">Casa de Jóvenes Levántate y Camina</input><br>
-          <input type="checkbox" name="institucion" value="car">Cáritas</input><br>
-          <input type="checkbox" name="institucion" value="mis">Grupo Misionero</input><br>
-          <br>
-        Educacion:
-        <select name="educacion">
-            <option value="sin-estudios">Sin estudios</option>
-            <option value="primario">Primario Completo</option>
-            <option value="secundario" selected="selected">Secundaria Completa</option>
-            <option value="formacion-profesional">Formación profesional</option>
-            <option value="universidad">Universidad</option>
-        </select>
-        <br><br>
-        Tenés título:
-          <input type="radio" name="titulo" value="si">Sí</input>
-          <input type="radio" name="titulo" value="no">No</input>
-          <input type="radio" name="titulo" value="tr">En trámite</input>
-        <br><br>
-        Profesión:
-        <input type="text" name="profesion" maxlength="50"><br>
-        <br>
-        Áreas de Interés:
-          <br>
-          <input type="checkbox" name="interes" value="tr">Ofertas de Empleo</input><br>
-          <input type="checkbox" name="interes"  value="cu">Cursos y Capacitaciones</input><br>
-          <input type="checkbox" name="interes" value="em">Emprendimientos</input><br>
-          <input type="checkbox" name="interes" value="ti">Obtener Título</input><br>
-          <input type="checkbox" name="interes" value="ay">Quiero Ayudar</input><br>
-          <input type="checkbox" name="interes" value="re">Recreación</input><br>
-        <br>
-        Dejanos tus comentarios:
-        <textarea name="comentarios" rows="5" cols="40"></textarea>
-        <br><br>
         <button type="submit"><strong>Registrarme</strong></button>
         <button type="reset"><img src="" alt=""><strong>Cancelar</strong></button>
 
-      </form>
+          </form>
       </section>
+
     </main>
   </body>
 </html>
